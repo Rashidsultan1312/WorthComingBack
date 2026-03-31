b34e409338be786fd7aa2c63507dbed0806bd030
keshew <artyom.korotckov2017@yandex.ru>
2026-03-31 16:54:23 +0500

    Initial Commit

---
+++ Worth Coming Back/Worth_Coming_BackApp.swift
@@ -0,0 +1,17 @@
+//
+//  Worth_Coming_BackApp.swift
+//  Worth Coming Back
+//
+//  Created by Артём Коротков on 31.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct Worth_Coming_BackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
